5c600c8b0d43472e40814f331aef3835f5f25ba9
BuildTools <unconfigured@null.spigotmc.org>
2023-02-17 08:06:29 +0300

    first

---
+++ main.py
@@ -0,0 +1,30 @@
+import sqlite3
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("main.ui", self)
+
+        self.con = sqlite3.connect("coffee.sqlite")
+        self.cur = self.con.cursor()
+
+        res = self.cur.execute("SELECT * FROM coffee").fetchall()
+        self.tableWidget.setColumnCount(7)
+        self.tableWidget.setRowCount(0)
+        self.tableWidget.setHorizontalHeaderLabels(['ID', 'Название', 'Обжарка', 'Молотый?', 'Вкус', 'Цена', 'Объем'])
+        for i, row in enumerate(res):
+            self.tableWidget.setRowCount(self.tableWidget.rowCount() + 1)
+            for j, elem in enumerate(row):
+                self.tableWidget.setItem(i, j, QTableWidgetItem(str(elem)))
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
